8de4684a58b34d3942b28a77879a0a40f99a1edc
Carlos Moreira <cesmoreira90@hotmail.com>
2024-10-24 09:09:07 -0300

    add cors and headers in vercel

--- src/routes.ts
+++ src/routes.ts
@@ -13,9 +13,19 @@ dotenv.config();
 
 const app = express();
 
+
+const options = [
+    cors({
+        origin: '*',
+        methods: '*',
+        allowedHeaders: ['Content-Type', 'Authorization',"x-tenant-id"],
+        credentials: true,
+    })
+];
+
+app.use(options);
 app.use(cors());
 app.use(express.json());
-
 app.use('/auth', authRoutes);
 app.use('/admin', adminRoutes);
 app.use('/api', uploadRoutes);
