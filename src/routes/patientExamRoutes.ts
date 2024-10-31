ecb6a407118cfa40feb712acd017f9840c9eb2f5
Carlos Moreira <cesmoreira90@hotmail.com>
2024-10-30 22:41:24 -0300

    add authmiddleware in the route

--- src/routes/patientExamRoutes.ts
+++ src/routes/patientExamRoutes.ts
@@ -7,9 +7,9 @@ import { createPatientExamController, listPatientExamsController, updatePatientE
 const router = Router();
 
 router.post('/', tenantMiddleware, authMiddleware, isAdminMiddleware, createPatientExamController);
-router.get('/', tenantMiddleware, listPatientExamsController);
-router.put('/:examId', tenantMiddleware, updatePatientExamController);
-router.delete('/:examId', tenantMiddleware, authMiddleware, isAdminMiddleware, deletePatientExamController);
+router.get('/', tenantMiddleware, authMiddleware, listPatientExamsController);
+router.put('/:patientExamId', tenantMiddleware, authMiddleware, isAdminMiddleware, updatePatientExamController);
+router.delete('/:patientExamId', tenantMiddleware, authMiddleware, isAdminMiddleware, deletePatientExamController);
 
 
 export default router;
