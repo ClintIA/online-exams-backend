b5043277980389de903d64da17fb62f677d66167
Carlos Moreira <cesmoreira90@hotmail.com>
2024-11-05 19:13:19 -0300

    fixing swagger

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,16 @@ import app from "./routes";
 import {connectDatabase} from "./config/database";
 
 const PORT = process.env.PORT || 3000;
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.0/swagger-ui.min.css";
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
 
 const startServer = async () => {
     try {
         app.use(bodyParser.json());
-        app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument,{ customCssUrl: CSS_URL }));
+        app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument,{
+            customCss:
+                '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+            customCssUrl: CSS_URL
+        }));
 
         await connectDatabase();
         console.log('Database Connected');
