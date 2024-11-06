2d26c41251bf166d076c59e535c4ff8723d5ab3c
Carlos Moreira <cesmoreira90@hotmail.com>
2024-11-06 10:54:08 -0300

    ssl false

--- src/config/database.ts
+++ src/config/database.ts
@@ -21,7 +21,7 @@ export const AppDataSource = new DataSource({
     entities: [Admin, NoticeCard, Patient, PatientClinic, Tenant, Product, TenantExams, PatientExams],
     synchronize: true,
     logging: true,
-    ssl: true,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 export const connectDatabase = async () => {
