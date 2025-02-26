95cb29f3b5fa7fc325a5334c8aadbb06f8a37a8e
Gabriel Krapp <gabriel.krapp@hotmail.com>
2025-02-26 10:17:22 -0300

    removing role default validation

--- src/middlewares/isAdminMiddleware.ts
+++ src/middlewares/isAdminMiddleware.ts
@@ -2,8 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { errorResponse } from '../utils/httpResponses';
 
 export const isAdminMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    if (req.user?.role == 'patient' ||
-        req.user?.role == 'default') {
+    if (req.user?.role == 'patient') {
         return errorResponse(res, new Error('Acesso negado: Apenas administradores podem realizar esta ação'), 403);
     }
     next();
