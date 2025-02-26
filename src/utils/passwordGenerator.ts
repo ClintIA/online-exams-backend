9714e7e8443f9031139b8248a7a490fe9804f724
Gabriel Krapp <gabriel.krapp@hotmail.com>
2025-02-26 09:53:38 -0300

    fix: password generator

--- src/utils/passwordGenerator.ts
+++ src/utils/passwordGenerator.ts
@@ -1,15 +1,21 @@
-export const generatePassword = (patientData: { full_name: string, dob?: string}) => {
-    const nome = patientData.full_name.toLowerCase().replace(/\s/g, '')
-    const dataNascimento = patientData.dob?.replace(/\D/g, '') || new Date().getFullYear().toString()
+export const generatePassword = (patientData: { full_name: string, dob?: string }) => {
+    const nome = patientData.full_name
+        .toLowerCase()
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+        .replace(/\s/g, '');
+    const dataNascimento = patientData.dob?.replace(/\D/g, '') || new Date().getFullYear().toString();
 
-    return nome.toLowerCase() +
-        dataNascimento.substring(0, 4)
+    return nome + dataNascimento.substring(0, 4);
 }
 
 export const generatePasswordByCpfAndName = (cpf: string, name: string) => {
-    const nome = name.toLowerCase().replace(/\s/g, '')
-    const cpfNumber = cpf.replace(/\D/g, '')
+    const nome = name
+        .toLowerCase()
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+        .replace(/\s/g, '');
+    const cpfNumber = cpf.replace(/\D/g, '');
 
-    return nome.toLowerCase() +
-        cpfNumber.substring(0, 4)
+    return nome + cpfNumber.substring(0, 4);
 }
