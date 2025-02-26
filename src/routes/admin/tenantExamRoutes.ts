f2d84ee17b5f44a71f2a8cd422d79a5e4192a5f6
Gabriel Krapp <gabriel.krapp@hotmail.com>
2025-02-26 10:14:24 -0300

    fix: removing admin permission to tenantExam list

--- src/routes/admin/tenantExamRoutes.ts
+++ src/routes/admin/tenantExamRoutes.ts
@@ -7,7 +7,7 @@ import { isAdminMiddleware } from '../../middlewares/isAdminMiddleware';
 const router = Router();
 
 router.post('/tenantexam', tenantMiddleware, authMiddleware, isAdminMiddleware, createExamController);
-router.get('/tenantexam', tenantMiddleware, authMiddleware, isAdminMiddleware, getExamsController);
+router.get('/tenantexam', tenantMiddleware, authMiddleware, getExamsController);
 router.put('/tenantexam/:clinicExamId', tenantMiddleware, authMiddleware, isAdminMiddleware, updateExamController);
 router.delete('/tenantexam/:clinicExamId', tenantMiddleware, authMiddleware, isAdminMiddleware, deleteExamController);
 
